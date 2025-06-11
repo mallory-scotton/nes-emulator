ce60caeba9c3ebaf7b14c9c95a5133434f9d92a5
mallory-scotton <mallory.scotton@epitech.eu>
2025-06-12 01:38:27 +0200

    add: adding base tests

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,12 +1,31 @@
 ///////////////////////////////////////////////////////////////////////////////
 // Dependencies
 ///////////////////////////////////////////////////////////////////////////////
-#include "Utils/Types.hpp"
+#include "Core/Cartridge.hpp"
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
 
 ///////////////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[])
 {
-    (void)argc; (void)argv;
+    if (argc < 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <path_to_rom>" << std::endl;
+        return (1);
+    }
 
+    try
+    {
+        NES::Cartridge cartridge(argv[1]);
+        std::cout << "Cartridge loaded successfully!" << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return (1);
+    }
     return (0);
 }
