bfa6a31ba9d532db569c822eb8560d0e69dc8c1d
mallory-scotton <mallory.scotton@epitech.eu>
2025-06-12 17:31:22 +0200

    update: updating mirroring type to handle SINGLE SCREEN lower and upper

--- Source/Core/Enums.hpp
+++ Source/Core/Enums.hpp
@@ -14,22 +14,25 @@ namespace NES
 ///
 /// This enumeration is used to specify how the memory mirroring is handled
 /// in the NES system. It defines four types of mirroring:
-/// - HORIZONTAL:   The memory is mirrored horizontally, meaning the left side
-///                 of the screen is a mirror image of the right side.
-/// - VERTICAL:     The memory is mirrored vertically, meaning the top half of
-///                 the screen is a mirror image of the bottom half.
-/// - FOUR_SCREEN:  The memory is mirrored in four different ways, allowing
-///                 for more complex graphics.
-/// - SINGLE_SCREEN:The memory is mirrored in a single way, meaning there
-///                 is no mirroring at all.
+/// - HORIZONTAL: The memory is mirrored horizontally, meaning the left side
+///               of the screen is a mirror image of the right side.
+/// - VERTICAL: The memory is mirrored vertically, meaning the top half of
+///             the screen is a mirror image of the bottom half.
+/// - FOUR_SCREEN: The memory is mirrored in four different ways, allowing
+///                for more complex graphics.
+/// - LOWER_SINGLE_SCREEN: The memory is mirrored to only the lower half of
+///                        the screen, allowing for a single-screen display.
+/// - UPPER_SINGLE_SCREEN: The memory is mirrored to only the upper half of
+///                        the screen, allowing for a single-screen display.
 ///
 ///////////////////////////////////////////////////////////////////////////////
 enum class MirroringType
 {
-    HORIZONTAL,     //<! Horizontal mirroring
-    VERTICAL,       //<! Vertical mirroring
-    FOUR_SCREEN,    //<! Four-screen mirroring
-    SINGLE_SCREEN   //<! Single-screen mirroring
+    HORIZONTAL,             //<! Horizontal mirroring
+    VERTICAL,               //<! Vertical mirroring
+    FOUR_SCREEN,            //<! Four-screen mirroring
+    LOWER_SINGLE_SCREEN,    //<! Lower Single-screen mirroring
+    UPPER_SINGLE_SCREEN     //<! Upper Single-screen mirroring
 };
 
 } // !namespace NES
