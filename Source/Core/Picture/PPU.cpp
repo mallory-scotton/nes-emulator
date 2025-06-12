fa0bd2a5578c8de7784c960881332a260598f431
mallory-scotton <mallory.scotton@epitech.eu>
2025-06-13 00:24:14 +0200

    add: adding pre-render rendering step for the PPU and reset state

--- Source/Core/Picture/PPU.cpp
+++ Source/Core/Picture/PPU.cpp
@@ -33,4 +33,108 @@ const std::vector<Byte>& PPU::GetScreen(void) const
     return (m_screen);
 }
 
+///////////////////////////////////////////////////////////////////////////////
+void PPU::Step(void)
+{
+    // Switch based on the current pipeline state
+    switch (m_pipelineState)
+    {
+    case State::PRE_RENDER:
+        PreRenderStep();
+        break;
+    case State::RENDER:
+        RenderStep();
+        break;
+    case State::POST_RENDER:
+        PostRenderStep();
+        break;
+    case State::VERTICAL_BLANK:
+        VerticalBlankStep();
+        break;
+    }
+
+    // Increment cycle and scanline
+    m_cycle++;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void PPU::Reset(void)
+{
+    m_longSprites = false;
+    m_generateInterrupt = false;
+    m_greyscaleMode = false;
+    m_vblank = false;
+    m_spriteOverflow = false;
+    m_showBackground = true;
+    m_showSprites = true;
+    m_evenFrame = true;
+    m_firstWrite = true;
+    m_dataAddress = 0x00;
+    m_cycle = 0;
+    m_scanline = 0;
+    m_spriteDataAddress = 0x00;
+    m_fineXScroll = 0;
+    m_tempAddress = 0x00;
+    m_dataAddrIncrement = 1;
+    m_pipelineState = State::PRE_RENDER;
+    m_scanlineSprites.reserve(8);
+    m_scanlineSprites.resize(0);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void PPU::PreRenderStep(void)
+{
+    bool showActors = m_showBackground && m_showSprites;
+
+    if (m_cycle == 1)
+    {
+        m_vblank = false;
+        m_sprZeroHit = false;
+    }
+    else if (m_cycle == SCANLINE_VISIBLE_DOTS + 2 && showActors)
+    {
+        // Set bits related to horizontal position
+        m_dataAddress &= ~0x41F;
+        m_dataAddress |= m_tempAddress & 0x41F;
+    }
+    else if (m_cycle > 280 && m_cycle <= 304 && showActors)
+    {
+        // Set bits related to vertical position
+        m_dataAddress &= ~0x7BE0;
+        m_dataAddress |= m_tempAddress & 0x7BE0;
+    }
+
+    // If we are at the end of the pre-render scanline
+    if (m_cycle >= SCANLINE_END_CYCLE - (!m_evenFrame && showActors))
+    {
+        m_pipelineState = State::RENDER;
+        m_cycle = 0;
+        m_scanline = 0;
+    }
+
+    // Handle IRQ for MMC3 mapper
+    if (m_cycle == 260 && showActors)
+    {
+        m_bus.ScanlineIRQ();
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void PPU::RenderStep(void)
+{
+    // TODO: Implement rendering logic
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void PPU::PostRenderStep(void)
+{
+    // TODO: Implement post-render logic
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void PPU::VerticalBlankStep(void)
+{
+    // TODO: Implement vertical blank logic
+}
+
 } // !namespace NES
