b41fe465825450fb840c856f8709df01e52f9800
mallory-scotton <mallory.scotton@epitech.eu>
2025-06-13 21:46:00 +0200

    add: adding Main bus processor

---
+++ Source/Core/Processor/MainBus.cpp
@@ -0,0 +1,191 @@
+///////////////////////////////////////////////////////////////////////////////
+// Dependencies
+///////////////////////////////////////////////////////////////////////////////
+#include "Core/Processor/MainBus.hpp"
+
+///////////////////////////////////////////////////////////////////////////////
+// Namespace NES
+///////////////////////////////////////////////////////////////////////////////
+namespace NES
+{
+
+///////////////////////////////////////////////////////////////////////////////
+MainBus::MainBus(
+    PPU& ppu,
+    APU& apu,
+    Controller& controller1,
+    Controller& controller2,
+    std::function<void(Byte)> callback
+)
+    : m_ram(0x800, 0x00)
+    , m_extRam(0x2000, 0x00)
+    , m_dmaCallback(std::move(callback))
+    , m_ppu(ppu)
+    , m_apu(apu)
+    , m_controller1(controller1)
+    , m_controller2(controller2)
+{}
+
+///////////////////////////////////////////////////////////////////////////////
+Address MainBus::NormalizeMirror(Address address)
+{
+    // Normalize the address to handle mirroring for PPU registers
+    if (address >= PPU_CTRL && address < APU_REGISTER_START)
+    {
+        return (address & 0x2007);
+    }
+    return (address);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+Byte MainBus::Read(Address address)
+{
+    if (address < 0x2000)
+    {
+        return (m_ram[address & 0x7FF]);
+    }
+    else if (address < 0x4020)
+    {
+        address = NormalizeMirror(address);
+        switch (static_cast<Register>(address))
+        {
+        case PPU_CTRL:
+            return (m_ppu.GetStatus());
+        case PPU_DATA:
+            return (m_ppu.GetData());
+        case JOY1:
+            return (m_controller1.Read());
+        case JOY2_AND_FRAME_CONTROL:
+            return (m_controller2.Read());
+        case OAM_DATA:
+            return (m_ppu.GetOAMData());
+        case APU_CONTROL_AND_STATUS:
+            return (m_apu.ReadStatus());
+        default:
+            return (0x00); // Unhandled register, return 0
+        }
+    }
+    else if (address < 0x6000)
+    {
+        // Expansion ROM or other memory-mapped devices
+        return (0x00);
+    }
+    else if (address < 0x8000)
+    {
+        return (m_extRam[address - 0x6000]);
+    }
+    else if (m_mapper)
+    {
+        return (m_mapper->ReadPGR(address));
+    }
+    return (0x00);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void MainBus::Write(Address address, Byte value)
+{
+    if (address < 0x2000)
+    {
+        m_ram[address & 0x7FF] = value;
+    }
+    else if (address < 0x4020)
+    {
+        address = NormalizeMirror(address);
+
+        switch (static_cast<Register>(address))
+        {
+        case PPU_CTRL:
+            m_ppu.Control(value);
+            break;
+        case PPU_MASK:
+            m_ppu.SetMask(value);
+            break;
+        case OAM_ADDR:
+            m_ppu.SetOAMAddress(value);
+            break;
+        case OAM_DATA:
+            m_ppu.SetOAMData(value);
+            break;
+        case PPU_ADDR:
+            m_ppu.SetDataAddress(value);
+            break;
+        case PPU_SCROLL:
+            m_ppu.SetScroll(value);
+            break;
+        case PPU_DATA:
+            m_ppu.SetData(value);
+            break;
+        case OAM_DMA:
+            m_dmaCallback(value);
+            break;
+        case JOY1:
+            m_controller1.Strobe(value);
+            m_controller2.Strobe(value);
+            break;
+        case APU_CONTROL_AND_STATUS:
+        case JOY2_AND_FRAME_CONTROL:
+            m_apu.WriteRegister(address, value);
+            break;
+        default:
+            if (address >= APU_REGISTER_START && address <= APU_REGISTER_END)
+            {
+                m_apu.WriteRegister(address, value);
+            }
+            break;
+        }
+    }
+    else if (address < 0x6000)
+    {
+        // Expansion ROM or other memory-mapped devices
+        // No write operation defined, do nothing
+    }
+    else if (address < 0x8000)
+    {
+        m_extRam[address - 0x6000] = value;
+    }
+    else if (m_mapper)
+    {
+        m_mapper->WritePGR(address, value);
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+bool MainBus::SetMapper(std::shared_ptr<Mapper> mapper)
+{
+    if (!mapper)
+    {
+        return (false);
+    }
+    m_mapper = mapper;
+    return (true);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+const Byte* MainBus::GetPagePtr(Byte page)
+{
+    Address address = page << 8;
+
+    if (address < 0x2000)
+    {
+        return (&m_ram[address & 0x7FF]);
+    }
+    else if (address < 0x4020)
+    {
+        // Register address memory pointer access attempt
+    }
+    else if (address < 0x6000)
+    {
+        // Expansion ROM access attempted, which is unsupported
+    }
+    else if (address < 0x8000)
+    {
+        return (&m_extRam[address - 0x6000]);
+    }
+    else
+    {
+        // Unexpected DMA request
+    }
+    return (nullptr);
+}
+
+} // !namespace NES
--- Source/Core/Processor/MainBus.hpp
+++ Source/Core/Processor/MainBus.hpp
@@ -10,6 +10,8 @@
 #include "Core/Picture/PPU.hpp"
 #include "Core/Audio/APU.hpp"
 #include "Core/Controller.hpp"
+#include <vector>
+#include <functional>
 
 ///////////////////////////////////////////////////////////////////////////////
 // Namespace NES
@@ -32,7 +34,7 @@ public:
     /// \brief Enumeration of memory-mapped registers
     ///
     ///////////////////////////////////////////////////////////////////////////
-    enum class Register
+    enum Register
     {
         PPU_CTRL = 0x2000,
         PPU_MASK = 0x2001,
@@ -49,6 +51,88 @@ public:
         JOY1 = 0x4016,
         JOY2_AND_FRAME_CONTROL = 0x4017
     };
+
+private:
+    ///////////////////////////////////////////////////////////////////////////
+    // Private members
+    ///////////////////////////////////////////////////////////////////////////
+    std::vector<Byte> m_ram;                    //<! Internal RAM
+    std::vector<Byte> m_extRam;                 //<! External RAM
+    std::function<void(Byte)> m_dmaCallback;    //<! DMA callback function
+    PPU& m_ppu;                                 //<! Reference to the PPU
+    APU& m_apu;                                 //<! Reference to the APU
+    Controller& m_controller1;                  //<! First controller
+    Controller& m_controller2;                  //<! Second controller
+
+public:
+    ///////////////////////////////////////////////////////////////////////////
+    /// \brief
+    ///
+    /// \param ppu
+    /// \param apu
+    /// \param controller1
+    /// \param controller2
+    /// \param callback
+    ///
+    ///////////////////////////////////////////////////////////////////////////
+    MainBus(
+        PPU& ppu,
+        APU& apu,
+        Controller& controller1,
+        Controller& controller2,
+        std::function<void(Byte)> callback
+    );
+
+public:
+    ///////////////////////////////////////////////////////////////////////////
+    /// \brief Read a byte from the bus
+    ///
+    /// \param address Address to read from
+    ///
+    /// \return The byte read from the bus
+    ///
+    ///////////////////////////////////////////////////////////////////////////
+    virtual Byte Read(Address address) override;
+
+    ///////////////////////////////////////////////////////////////////////////
+    /// \brief Write a byte to the bus
+    ///
+    /// \param address Address to write to
+    /// \param value Value to write
+    ///
+    ///////////////////////////////////////////////////////////////////////////
+    virtual void Write(Address address, Byte value) override;
+
+    ///////////////////////////////////////////////////////////////////////////
+    /// \brief Set the mapper for the bus
+    ///
+    /// \param mapper Shared pointer to the mapper to set
+    ///
+    /// \return True if the mapper was set successfully, false otherwise
+    ///
+    ///////////////////////////////////////////////////////////////////////////
+    virtual bool SetMapper(std::shared_ptr<Mapper> mapper) override;
+
+    ///////////////////////////////////////////////////////////////////////////
+    /// \brief Get the pointer to a specific page in the RAM
+    ///
+    /// \param page Page number to get the pointer for
+    ///
+    /// \return Pointer to the start of the specified page in RAM
+    ///
+    ///////////////////////////////////////////////////////////////////////////
+    const Byte* GetPagePtr(Byte page);
+
+private:
+    ///////////////////////////////////////////////////////////////////////////
+    /// \brief Normalize a mirror address
+    ///
+    /// \param address Address to normalize
+    ///
+    /// \return The normalized address
+    ///
+    ///////////////////////////////////////////////////////////////////////////
+    static Address NormalizeMirror(Address address);
 };
 
 } // !namespace NES
