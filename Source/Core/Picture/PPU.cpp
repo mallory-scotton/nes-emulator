cea65c87e54770773500bb73edcb5a64e122228a
mallory-scotton <mallory.scotton@epitech.eu>
2025-06-13 18:48:18 +0200

    add: adding render step for the picture processing unit

--- Source/Core/Picture/PPU.cpp
+++ Source/Core/Picture/PPU.cpp
@@ -2,6 +2,7 @@
 // Dependencies
 ///////////////////////////////////////////////////////////////////////////////
 #include "Core/Picture/PPU.hpp"
+#include "Core/Colors.hpp"
 
 ///////////////////////////////////////////////////////////////////////////////
 // Namespace NES
@@ -127,7 +128,226 @@ void PPU::PreRenderStep(void)
 ///////////////////////////////////////////////////////////////////////////////
 void PPU::RenderStep(void)
 {
-    // TODO: Implement rendering logic
+    if (m_cycle > 0 && m_cycle <= SCANLINE_VISIBLE_DOTS)
+    {
+        Byte bgColor = 0;
+        Byte sprColor = 0;
+        bool bgOpaque = false;
+        bool sprOpaque = true;
+        bool spriteForeground = false;
+
+        int x = m_cycle - 1;
+        int y = m_scanline;
+
+        if (m_showBackground)
+        {
+            int x_fine = (m_fineXScroll + x) % 8;
+
+            if (!m_hideEdgeBackground || x >= 8)
+            {
+                int addr = 0x2000 | (m_dataAddress & 0x0FFF);
+                Byte tile = m_bus.Read(addr);
+
+                addr = (tile * 16) + ((m_dataAddress >> 12) & 0x7);
+                addr |= static_cast<int>(m_bgPage) << 12;
+                bgColor = (m_bus.Read(addr) >> (7 ^ x_fine)) & 1;
+                bgColor |= ((m_bus.Read(addr + 8) >> (7 ^ x_fine)) & 1) << 1;
+
+                bgOpaque = bgColor;
+
+                addr = 0x23C0 |
+                    (m_dataAddress & 0x0C00) |
+                    ((m_dataAddress >> 4) & 0x38) |
+                    ((m_dataAddress >> 2) & 0x07);
+                Byte attribute = m_bus.Read(addr);
+                int shift = ((m_dataAddress >> 4) & 4) | (m_dataAddress & 2);
+                bgColor |= ((attribute >> shift) & 0x3) << 2;
+            }
+
+            if (x_fine == 7)
+            {
+                if ((m_dataAddress & 0x001F) == 31)
+                {
+                    m_dataAddress &= ~0x001F;
+                    m_dataAddress ^= 0x0400;
+                }
+                else
+                {
+                    m_dataAddress += 1;
+                }
+            }
+        }
+
+        if (m_showSprites && (!m_hideEdgeSprites || x >= 8))
+        {
+            for (Byte i : m_scanlineSprites)
+            {
+                Byte spr_x = m_spriteMemory[i * 4 + 3];
+
+                if (0 > x - spr_x || x - spr_x >= 8)
+                {
+                    continue;
+                }
+
+                Byte spr_y = m_spriteMemory[i * 4 + 0] + 1;
+                Byte tile = m_spriteMemory[i * 4 + 1];
+                Byte attribute = m_spriteMemory[i * 4 + 2];
+
+                int length = (m_longSprites) ? 16 : 8;
+
+                int x_shift = (x - spr_x) % 8;
+                int y_offset = (y - spr_y) % length;
+
+                if ((attribute & 0x40) == 0)
+                {
+                    x_shift ^= 7;
+                }
+                if ((attribute & 0x80) != 0)
+                {
+                    y_offset ^= (length - 1);
+                }
+
+                Address addr = 0;
+
+                if (!m_longSprites)
+                {
+                    addr = tile * 16 + y_offset;
+                    if (m_sprPage == CharacterPage::HIGH)
+                    {
+                        addr += 0x1000;
+                    }
+                }
+                else
+                {
+                    y_offset = (y_offset & 7) | ((y_offset & 8) << 1);
+                    addr = (tile >> 1) * 32 + y_offset;
+                    addr |= (tile & 1) << 12;
+                }
+
+                sprColor |= (m_bus.Read(addr) >> (x_shift)) & 1;
+                sprColor |= ((m_bus.Read(addr + 8) >> (x_shift)) & 1) << 1;
+
+                if (!(sprOpaque = sprColor))
+                {
+                    sprColor = 0;
+                    continue;
+                }
+
+                sprColor |= 0x10;
+                sprColor |= (attribute & 0x3) << 2;
+
+                spriteForeground = !(attribute & 0x20);
+
+                if (
+                    !m_sprZeroHit && m_showBackground &&
+                    i == 0 && sprOpaque && bgOpaque
+                )
+                {
+                    m_sprZeroHit = true;
+                }
+
+                break;
+            }
+        }
+
+        Byte paletteAddr = bgColor;
+
+        if (
+            (!bgOpaque && sprOpaque) ||
+            (bgOpaque && sprOpaque && spriteForeground)
+        )
+        {
+            paletteAddr = sprColor;
+        }
+        else if (!bgOpaque && !sprOpaque)
+        {
+            paletteAddr = 0;
+        }
+
+        int index = y * SCANLINE_VISIBLE_DOTS * 4 + x * 4;
+        Uint32 color = NES_COLORS[m_bus.ReadPalette(paletteAddr)];
+
+        m_buffer[index + 0] = (color >> 16) & 0xFF; // R
+        m_buffer[index + 1] = (color >> 8) & 0xFF;  // G
+        m_buffer[index + 2] = (color >> 0) & 0xFF;  // B
+        m_buffer[index + 3] = 0xFF;                 // A
+    }
+    else if (m_cycle == SCANLINE_VISIBLE_DOTS + 1 && m_showBackground)
+    {
+        if ((m_dataAddress & 0x7000) != 0x7000)
+        {
+            m_dataAddress += 0x1000;
+        }
+        else
+        {
+            m_dataAddress &= ~0x7000;
+            int y = (m_dataAddress & 0x03E0) >> 5;
+
+            if (y == 29)
+            {
+                y = 0;
+                m_dataAddress ^= 0x0800;
+            }
+            else if (y == 31)
+            {
+                y = 0;
+            }
+            else
+            {
+                y++;
+            }
+
+            m_dataAddress = (m_dataAddress & ~0x03E0) | (y << 5);
+        }
+    }
+    else if (
+        m_cycle == SCANLINE_VISIBLE_DOTS + 2 &&
+        m_showBackground && m_showSprites
+    )
+    {
+        m_dataAddress &= ~0x41F;
+        m_dataAddress |= m_tempAddress & 0x41F;
+    }
+
+    if (m_cycle == 260 && m_showBackground && m_showSprites)
+    {
+        m_bus.ScanlineIRQ();
+    }
+
+    if (m_cycle >= SCANLINE_END_CYCLE)
+    {
+        m_scanlineSprites.resize(0);
+
+        int range = 8;
+        if (m_longSprites)
+        {
+            range = 16;
+        }
+
+        size_t j = 0;
+        for (size_t i = m_spriteDataAddress / 4; i < 64; i++)
+        {
+            int diff = (m_scanline - m_spriteMemory[i * 4]);
+            if (0 <= diff && diff < range)
+            {
+                if (j >= 8)
+                {
+                    m_spriteOverflow = true;
+                    break;
+                }
+                m_scanlineSprites.push_back(i);
+                j++;
+            }
+        }
+
+        m_scanline++;
+        m_cycle = 0;
+    }
+
+    if (m_scanline >= VISIBLE_SCANLINES)
+    {
+        m_pipelineState = State::POST_RENDER;
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
