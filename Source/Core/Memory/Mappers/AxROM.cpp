3d9810e6908c7135a034467e162c0423735c78b0
mallory-scotton <mallory.scotton@epitech.eu>
2025-06-25 00:36:26 +0200

    add: adding new mapper: AxROM

--- Source/Core/Memory/Mappers/AxROM.cpp
+++ Source/Core/Memory/Mappers/AxROM.cpp
@@ -11,40 +11,58 @@ namespace NES::Mappers
 {
 
 ///////////////////////////////////////////////////////////////////////////////
-AxROM::AxROM(Cartridge& cartridge)
+AxROM::AxROM(Cartridge& cartridge, std::function<void(void)> callback)
     : Mapper(cartridge, 7)
-{}
+    , m_mirroring(MirroringType::LOWER_SINGLE_SCREEN)
+    , m_callback(callback)
+    , m_pgrBank(0)
+{
+    if (m_cartridge.GetCHR().size() == 0)
+    {
+        m_ram.resize(0x2000, 0x00);
+    }
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 Byte AxROM::ReadPGR(Address address)
 {
-    NES_UNUSED(address);
-    // TODO: Implement PGR read logic
+    if (address >= 0x8000)
+    {
+        return (m_cartridge.GetPGR()[m_pgrBank * 0x8000 + (address & 0x7FFF)]);
+    }
     return (0x00);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 void AxROM::WritePGR(Address address, Byte value)
 {
-    NES_UNUSED(address);
-    NES_UNUSED(value);
-    // TODO: Implement PGR write logic
+    if (address >= 0x8000)
+    {
+        m_pgrBank = value & 0x07;
+        m_mirroring = (value & 0x10) ?
+            MirroringType::UPPER_SINGLE_SCREEN :
+            MirroringType::LOWER_SINGLE_SCREEN;
+        m_callback();
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 Byte AxROM::ReadCHR(Address address)
 {
-    NES_UNUSED(address);
-    // TODO: Implement CHR read logic
+    if (address < 0x2000)
+    {
+        return (m_ram[address]);
+    }
     return (0x00);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 void AxROM::WriteCHR(Address address, Byte value)
 {
-    NES_UNUSED(address);
-    NES_UNUSED(value);
-    // TODO: Implement CHR write logic
+    if (address < 0x2000)
+    {
+        m_ram[address] = value;
+    }
 }
 
 } // !namespace NES::Mappers
